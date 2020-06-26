050aaf0f0f6a189f2103b215295b5e54a79fffc9
Bradley Baysinger <bradleybaysinger@bradleys-pro.lan>
2020-06-26 16:18:33 -0700

    Added styles for wheel component.

--- src/wiss-wheel/WissWheel.jsx
+++ src/wiss-wheel/WissWheel.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import ButtonRing from './ButtonRing';
+import './WissWheel.scss';
 
 export default class WissWheel extends React.Component {
 
@@ -16,7 +17,7 @@ export default class WissWheel extends React.Component {
   }
 
   render() {
-    return <svg width="500" height="500">
+    return <svg className="wiss-wheel" width="500" height="500">
       <g>
         <ButtonRing id="ring1" data={this.buttonData[0]} />
         <ButtonRing id="ring2" data={this.buttonData[1]} />
