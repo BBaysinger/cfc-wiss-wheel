134c68b46dd551728b7b597e54e85fbc22d20bb4
Bradley Baysinger <bradleybaysinger@bradleys-pro.lan>
2020-06-26 17:31:39 -0700

    Formatted all files (preference as it helps with versioning.

--- src/wiss-wheel/Button.jsx
+++ src/wiss-wheel/Button.jsx
@@ -5,8 +5,10 @@ export default class Test extends React.Component {
   constructor() {
     super();
   }
-  
+
   render() {
-    return <path />
+    return <g>
+      <circle cx="0" cy="0" r="200" stroke="black" stroke-width="3" fill="transparent" />
+    </g>
   }
 }
--- src/wiss-wheel/WissWheel.jsx
+++ src/wiss-wheel/WissWheel.jsx
@@ -5,11 +5,15 @@ import './WissWheel.scss';
 
 export default class WissWheel extends React.Component {
 
+  static HEIGHT = 800;
+  static WIDTH = 800;
+  static VIEWBOX = -WissWheel.HEIGHT / 2 + " " + -WissWheel.WIDTH / 2 + " " + WissWheel.HEIGHT + " " + WissWheel.WIDTH;
+
   buttonData = [
-    {radius: 100, stroke: 50 , ids:["Families", "", "", ""]},
-    {radius: 150, stroke: 75 , ids:["Elementary", "Middle School", "HighSchool", "Early Learning"]},
-    {radius: 100, stroke: 150, ids:["SEL for Adults", "", ""]},
-    {radius: 100, stroke: 250, ids:["Out-of-School Time"]},
+    { radius: 100, stroke: 50, ids: ["Families", "", "", ""] },
+    { radius: 150, stroke: 75, ids: ["Elementary", "Middle School", "HighSchool", "Early Learning"] },
+    { radius: 100, stroke: 150, ids: ["SEL for Adults", "", ""] },
+    { radius: 100, stroke: 250, ids: ["Out-of-School Time"] },
   ];
 
   constructor() {
@@ -17,7 +21,7 @@ export default class WissWheel extends React.Component {
   }
 
   render() {
-    return <svg className="wiss-wheel" xmlns="http://www.w3.org/2000/svg" width="500" height="500">
+    return <svg className="wiss-wheel" viewBox={WissWheel.VIEWBOX} xmlns="http://www.w3.org/2000/svg" width={WissWheel.HEIGHT} height={WissWheel.WIDTH}>
       <g>
         <ButtonRing id="ring1" data={this.buttonData[0]} />
         <ButtonRing id="ring2" data={this.buttonData[1]} />
