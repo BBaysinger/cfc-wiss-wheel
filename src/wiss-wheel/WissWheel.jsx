ee5f4d9abeddb658ae04167e69f08ebe93245cd8
Bradley Baysinger <bradleybaysinger@bradleys-pro.lan>
2020-06-30 01:47:18 -0700

    Passable for preso.

--- src/wiss-wheel/WissWheel.jsx
+++ src/wiss-wheel/WissWheel.jsx
@@ -35,7 +35,7 @@ export default class WissWheel extends React.Component {
     },
     {
       radius: 287.5, thickness: 75, buttonConfigs: [
-        {},
+        {  },
         { color: "#B1B3B6", textColor: '#fff', label: "SEL for Adults" },
         { color: "#B1B3B6" },
         { color: "#B1B3B6" },
